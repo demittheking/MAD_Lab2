96c947957ed935fbc6eade5dcb52a0234b5ef849
demittheking <dkarasev42@gmail.com>
2023-04-05 18:28:40 +0600

    Made inner interface with a method inside the fragment. Added some Flags. I made a transition from a fragment to activity1 with help of a callback.

--- app/src/main/java/com/example/mad_lab2/MainActivity.kt
+++ app/src/main/java/com/example/mad_lab2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mad_lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab2_dma.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
